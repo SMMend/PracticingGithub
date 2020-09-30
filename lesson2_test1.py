b940e5ca0cb1f971a9ea29fed4d0aa5706361fa6
smmend <sybilmendonca@gmail.com>
2020-09-30 10:53:43 -0400

    Commiting my first test code

---
+++ lesson2_test1.py
@@ -0,0 +1,22 @@
+
+days_of_week=["Monday", "tuesday"]
+print days_of_week
+
+days_of_week.append("Wednesday")
+print days_of_week
+
+days_of_week.append("Thursday") #append allows you to apend just one value at a time
+days_of_week.append( "Friday")
+days_of_week.append( "Saturday")
+days_of_week.append("Sunday")
+print days_of_week
+print len(days_of_week)
+
+day = days_of_week.pop()
+print day
+print days_of_week
+
+day=days_of_week.pop(3)
+print day, days_of_week
+
+
